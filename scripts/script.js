7bd2f44d60943173cb2fe5c0da4fbe228f10a73c
Vera Chernushina <vera.chernushina@yandex.ru>
2021-10-18 01:19:48 +0300

    feat: change names of functions, add fade-out and fade-in effects to popup

--- scripts/script.js
+++ scripts/script.js
@@ -32,39 +32,33 @@ const profileEditBtn = document.querySelector('.profile__edit-button');
 // находим кнопку закрытия попапа редактирования профиля
 const popupEditCloseBtn = popupEditProfile.querySelector('.popup__close');
 // находим форму попапа редактирования профиля
-const editForm = popupEditProfile.querySelector('.popup__form');
+const formEditProfile = popupEditProfile.querySelector('.popup__form');
 // находим инпуты формы попапа редактирования профиля
-let nameInput = editForm.querySelector('#name');
-let jobInput = editForm.querySelector('#job');
+let nameInput = formEditProfile.querySelector('#name');
+let jobInput = formEditProfile.querySelector('#job');
 // выбираем куда будут импортироваться данные из формы
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__description');
 // Находим попап добавления карточки
-const addCardPopup = document.querySelector('.popup_type_new-card');
+const popupAddNewCard = document.querySelector('.popup_type_new-card');
 // находим кнопку для открытия попапа добавления новой карточки
-const openCardPopupBtn = document.querySelector('.profile__add-button');
+const popupAddNewCardOpenBtn = document.querySelector('.profile__add-button');
 // находим кнопку для закрытия попапа добавления новой карточки
-const closeCardPopupBtn = addCardPopup.querySelector('.popup__close');
-// находим кнопку "Создать" попапа добавления новой карточки
-const addNewCardBtn = addCardPopup.querySelector('.popup__submit-btn');
+const popupAddNewCardCloseBtn = popupAddNewCard.querySelector('.popup__close');
 // находим форму попапа добавления новой карточки
-const addCardForm = addCardPopup.querySelector('.popup__form');
+const formAddNewCard = popupAddNewCard.querySelector('.popup__form');
 // находим контейнер для карточек
 const cardsContainer = document.querySelector('.elements');
 // находим шаблон для создания карточки
 const cardTemplate = document.querySelector('.element-template').content;
 // находим инпут для названия карточки
-let cardNameInput = addCardForm.querySelector('#name');
+const cardNameInput = formAddNewCard.querySelector('#name');
 // находим инпут ссылки на изображение
-let cardLinkInput = addCardPopup.querySelector('#link');
-// переменная для кнопки лайк
-let cardLikeBtn;
-// переменная для кнопки удаления карточки
-let cardDeleteBtn;
+const cardLinkInput = popupAddNewCard.querySelector('#link');
 // находим попап просмотра изображения
-const viewImagePopup = document.querySelector('.popup_type_image');
+const popupViewImage = document.querySelector('.popup_type_image');
 // находим кнопку закрытия попапа просмотра изображений
-const viewImagePopupCloseBtn = viewImagePopup.querySelector('.popup__close');
+const popupViewImageCloseBtn = popupViewImage.querySelector('.popup__close');
 
 
 // функция показа/скрытия попапа
@@ -85,6 +79,7 @@ function submitEditFormHandler (evt) {
 
   togglePopup(popupEditProfile);
 }
+
 // функция добавления карточки
 const addCard = (nameValue, imgValue) => {
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
@@ -93,13 +88,13 @@ const addCard = (nameValue, imgValue) => {
   // находим название изображения
   const cardName = cardElement.querySelector('.element__title');
   // находим кнопку лайк карточки
-  cardLikeBtn = cardElement.querySelector('.element__like-btn');
+  const cardLikeBtn = cardElement.querySelector('.element__like-btn');
   // находим кнопку удаления карточки
-  cardDeleteBtn = cardElement.querySelector('.element__delete-btn');
+  const cardDeleteBtn = cardElement.querySelector('.element__delete-btn');
   // находим изображение попапа просмотра
-  const viewImagePopupImg = viewImagePopup.querySelector('.popup__img');
+  const viewImagePopupImg = popupViewImage.querySelector('.popup__img');
   // находим название изображения попапа просмотра
-  const viewImagePopupName = viewImagePopup.querySelector('.popup__caption');
+  const viewImagePopupName = popupViewImage.querySelector('.popup__caption');
 
   cardName.textContent = nameValue;
   cardImg.src = imgValue;
@@ -107,14 +102,14 @@ const addCard = (nameValue, imgValue) => {
 
   //функция открытия попапа просмотра изображений
   const openViewImagePopup = () => {
-    togglePopup(viewImagePopup);
+    togglePopup(popupViewImage);
 
     viewImagePopupImg.src = cardImg.src;
     viewImagePopupName.textContent = cardName.textContent;
     viewImagePopupImg.alt = cardName.textContent;
 
     // листенер закрытия попапа просмотра изображения
-    viewImagePopupCloseBtn.onclick = () => togglePopup(viewImagePopup);
+    popupViewImageCloseBtn.onclick = () => togglePopup(popupViewImage);
   };
 
   // обработчик кнопки лайк
@@ -148,17 +143,17 @@ popupEditCloseBtn.addEventListener('click', () => {
   togglePopup(popupEditProfile);
 });
 // Обработчик кнопки Submit попапа редактирования профиля
-editForm.addEventListener('submit', submitEditFormHandler);
+formEditProfile.addEventListener('submit', submitEditFormHandler);
 // листенер кнопки открытия попапа добавления новой карточки
-openCardPopupBtn.addEventListener('click', () => {
-  togglePopup(addCardPopup);
+popupAddNewCardOpenBtn.addEventListener('click', () => {
+  togglePopup(popupAddNewCard);
 });
 // листенер кнопки закрытия попапа добавления новой карточки
-closeCardPopupBtn.addEventListener('click', () => {
-  togglePopup(addCardPopup);
+popupAddNewCardCloseBtn.addEventListener('click', () => {
+  togglePopup(popupAddNewCard);
 });
-// листенер кнопки "Создать" карточку
-addNewCardBtn.addEventListener('click', (evt) => {
+// листенер submit формы созлания карточки
+formAddNewCard.addEventListener('submit', (evt) => {
   evt.preventDefault();
 
   const name = cardNameInput;
@@ -169,7 +164,7 @@ addNewCardBtn.addEventListener('click', (evt) => {
   name.value = "";
   img.value = "";
 
-  togglePopup(addCardPopup);
+  togglePopup(popupAddNewCard);
 });
 
 // автоматическая загрузка карточек на страницу
